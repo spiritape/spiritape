feb5d4cd4dbef512f3bd883b963002955047d107
Micah <spiritapes@gmail.com>
2015-11-04 14:08:55 -0800

    first commit

--- index.js
+++ index.js
@@ -1,21 +1,3 @@
-// var express = require('express');
-// var app = express();
-
-// app.set('port', (process.env.PORT || 5000));
-
-// app.use('/', express.static(__dirname));
-
-// // views is directory for all template files
-// app.set('views', __dirname + '/views');
-
-
-// app.get('/', function(request, response) {
-//   response.render('index.html');
-// });
-
-// app.listen(app.get('port'), function() {
-//   console.log('Node app is running on port', app.get('port'));
-// });
 
 var express = require('express');
 var app = express();
