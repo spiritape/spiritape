84a19d61a23cce048e460d7804b0d095bff07f8e
Micah <spiritapes@gmail.com>
2015-11-04 16:00:53 -0800

    basic outline

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ var app = express();
      res.sendfile( __dirname + req.params[0]); 
  });
  
- var port = process.env.PORT || 5001;
+ var port = process.env.PORT || 5000;
  app.listen(port, function() {
    console.log("Listening on " + port);
  })
